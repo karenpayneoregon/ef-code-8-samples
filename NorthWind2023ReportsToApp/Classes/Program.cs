147eeb5dd327163bd904b8631949f8e3aa1c57ad
Karen Payne <paynekaren@comcast.net>
2023-12-05 04:55:43 -0800

    tinkering

--- NorthWind2023ReportsToApp/Classes/Program.cs
+++ NorthWind2023ReportsToApp/Classes/Program.cs
@@ -9,8 +9,13 @@ partial class Program
     [ModuleInitializer]
     public static void Init()
     {
-        Console.Title = "Code sample - EF Core NorthWind 2023 Reports to";
+        string PathJoin(string text1, string text2, string text3, string separator = "/") 
+            => $"{text1}{separator}{text2}{separator}{text3}";
+
+        Console.Title = PathJoin("Code sample", "EF Core NorthWind 2023", "2023 Reports to");
         AnsiConsole.MarkupLine("[cyan]Reading employee information[/]");
+
         W.SetConsoleWindowPosition(W.AnchorWindow.Center);
+
     }
 }
