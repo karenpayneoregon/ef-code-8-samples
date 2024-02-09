2fd14b2025218fb5e972c020bf10cd48303c92b7
Karen Payne <paynekaren@comcast.net>
2024-02-09 03:52:42 -0800

    updates

--- UserDefineFunctionMapping1/Program.cs
+++ UserDefineFunctionMapping1/Program.cs
@@ -5,18 +5,18 @@ using UserDefineFunctionMapping1.Models;
 
 namespace UserDefineFunctionMapping1;
 
-    internal partial class Program
+internal partial class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
 
-            using var context = new Context();
-            const string excludeThisContact = "Patricio Simpson";
-            List<Contacts> list = context
-                .Contacts
-                .Include(c => c.ContactTypeIdentifierNavigation)
-                .Where(c => context.ConcatStrings(c.FirstName, c.LastName) != excludeThisContact)
-                .ToList();
+        using var context = new Context();
+        const string excludeThisContact = "Patricio Simpson";
+        List<Contacts> list = context
+            .Contacts
+            .Include(c => c.ContactTypeIdentifierNavigation)
+            .Where(c => context.ConcatStrings(c.FirstName, c.LastName) != excludeThisContact)
+            .ToList();
 
-        }
     }
+}
