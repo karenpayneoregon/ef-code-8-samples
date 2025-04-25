0dac7955bf6eeb1a1661601c6d4b50dbc632e3f2
Karen Payne <paynekaren@comcast.net>
2025-04-25 06:55:03 -0700

    Remove Serilog and refactor OnGet method
    Eliminate the use of the Serilog logging library and remove the commented-out `OnGet` method. Update the constructor to directly use the `context` parameter. Add a summary comment to the `OnGetAsync` method, which retrieves a list of cars from the database and assigns it to the `Cars` property, including their associated manufacturers.
    
    by Karen

--- UseSeedingSqlServerExample/Pages/Index.cshtml.cs
+++ UseSeedingSqlServerExample/Pages/Index.cshtml.cs
@@ -1,27 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using Serilog;
 using UseSeedingSqlServerExample.Data;
 using UseSeedingSqlServerExample.Models;
 
 namespace UseSeedingSqlServerExample.Pages;
 public class IndexModel(Context context) : PageModel
 {
-    private readonly Context _context = context;
-
     [BindProperty]
     public required List<Car> Cars { get; set; }
 
-    //public void OnGet()
-    //{
-    //    Log.Information("Car count {C}", _context.Car.Count());
 
-    //    var firstCar = _context.Car.Include(x => x.Manufacturer).FirstOrDefault();
-    //}
-
+    /// <summary>
+    /// This method retrieves a list of cars from the database, including their associated manufacturers,
+    /// and assigns it to the <see cref="Cars"/> property.
+    /// </summary>
     public async Task OnGetAsync()
     {
-        Cars = await _context.Car.Include(c => c.Manufacturer).ToListAsync();
+        Cars = await context.Car.Include(c => c.Manufacturer).ToListAsync();
     }
 }
