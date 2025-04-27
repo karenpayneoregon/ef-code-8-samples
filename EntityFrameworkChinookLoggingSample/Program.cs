ae923d37231419566ef63aaae20df2496bb2abe4
Karen Payne <paynekaren@comcast.net>
2025-04-27 05:22:13 -0700

    clean-up

--- EntityFrameworkChinookLoggingSample/Program.cs
+++ EntityFrameworkChinookLoggingSample/Program.cs
@@ -15,15 +15,6 @@ internal partial class Program
 
         int albumIdentifier = 129;
 
-        //var LedZeppelin_IV_Album = context
-        //    .Album
-        //    .AsNoTracking()
-        //    .AsSplitQuery()
-        //    .Include(a => a.Track.OrderBy(track => track.Name))
-        //    .Include(album => album.Artist)
-        //    .TagWith("LedZeppelin_IV_Album")
-        //    .FirstOrDefault(a => a.AlbumId == albumIdentifier);
-
         var ledZeppelinIvAlbum = context
             .Album
             .AsNoTracking()
@@ -46,34 +37,11 @@ internal partial class Program
         {
             AnsiConsole.MarkupLine($"[cyan]Artist[/] [yellow]{ledZeppelinIvAlbum.Artist.Name}[/]");
             AnsiConsole.MarkupLine($" [cyan]Title[/] [yellow]{ledZeppelinIvAlbum.Title}[/]");
-            Console.WriteLine();
-            //AnsiConsole.MarkupLine("[dodgerblue1]Song                               Time      Composer[/]");
-            AnsiConsole.MarkupLine("[dodgerblue1]    Song                               Time      Composer[/]");
-            //foreach (var track in ledZeppelinIvAlbum.Track)
-            //{
-            //    Console.WriteLine($"{track.Name,-35}{track.Milliseconds.ShowTime(),-10}{track.Composer}");
-            //}
 
-            //Console.WriteLine();
             foreach (var (track, index) in ledZeppelinIvAlbum.Track.Select((track, index) => (t: track, i: index)))
             {
                 Console.WriteLine($"{index,-4:D2}{track.Name,-35}{track.Milliseconds.ShowTime(),-10}{track.Composer}");
             }
-
-
-            //foreach (var (track, index) in ledZeppelinIvAlbum.Track.Select((track, index) => (value: track, i: index)))
-            //{
-
-            //}
-
-            //Console.WriteLine();
-
-            //var paths = Environment.GetEnvironmentVariable("Path")!.Split(";");
-            //foreach (var (part, index) in paths.Select((part, index) => (value: part, i: index)))
-            //{
-            //    Console.WriteLine($"{index,-3}{part}");
-            //}
-
 
         }
         else
