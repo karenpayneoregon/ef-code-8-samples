897b080f9dca3e7618397f49a963e8faa00348a9
Karen Payne <paynekaren@comcast.net>
2026-02-15 18:52:31 -0800

    Add DbContext query filter extension methods
    Introduce DbContextExtensions with HasQueryFilter and QueryFilterDefinition methods to inspect query filters for entity types. Update Program.cs to use these methods for Customer entities. Add null-check and error handling for missing countries. Update using directives for required namespaces.
    
    by Karen

---
+++ HasQueryFilterConditionalSample/Extensions/DbContextExtensions.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace HasQueryFilterConditionalSample.Extensions;
+
+public static class DbContextExtensions
+{
+    /// <summary>
+    /// Determines whether the specified entity type has a query filter applied in the current <see cref="DbContext"/>.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity to check for a query filter.</typeparam>
+    /// <param name="context">The <see cref="DbContext"/> instance to inspect.</param>
+    /// <returns>
+    /// <see langword="true"/> if the specified entity type has a query filter applied; otherwise, <see langword="false"/>.
+    /// </returns>
+    /// <remarks>
+    /// This method checks the model metadata of the provided <see cref="DbContext"/> to determine if a query filter
+    /// is defined for the specified entity type.
+    /// </remarks>
+    public static bool HasQueryFilter<TEntity>(this DbContext context)  where TEntity : class
+    {
+        var entityType = context.Model.FindEntityType(typeof(TEntity));
+        return entityType?.GetQueryFilter() != null;
+    }
+
+    /// <summary>
+    /// Retrieves the query filter definition for the specified entity type in the current <see cref="DbContext"/>.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity for which the query filter definition is retrieved.</typeparam>
+    /// <param name="context">The <see cref="DbContext"/> instance containing the entity type.</param>
+    /// <returns>
+    /// A string representation of the query filter definition if it exists; otherwise, "Unknown".
+    /// </returns>
+    /// <remarks>
+    /// This method checks the model metadata of the specified entity type to determine if a query filter is defined.
+    /// </remarks>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if the <paramref name="context"/> is <c>null</c>.
+    /// </exception>
+    public static string QueryFilterDefinition<TEntity>(this DbContext context) where TEntity : class
+    {
+        var entityType = context.Model.FindEntityType(typeof(TEntity));
+        var filter = entityType?.GetQueryFilter();
+        return filter?.ToString() ?? "Unknown";
+    }
+}
+
--- HasQueryFilterConditionalSample/Program.cs
+++ HasQueryFilterConditionalSample/Program.cs
@@ -3,6 +3,8 @@ using HasQueryFilterConditionalSample.Classes.Core;
 using HasQueryFilterConditionalSample.Data;
 using HasQueryFilterConditionalSample.Extensions;
 using Spectre.Console;
+using System.Xml;
+using HasQueryFilterConditionalSample.Models;
 
 namespace HasQueryFilterConditionalSample;
 internal partial class Program
@@ -21,18 +23,27 @@ internal partial class Program
         {
             var countryId = ContextSettings.Instance.CustomerOptions.CountryCode;
             var country = context.Countries.FirstOrDefault(c => c.CountryIdentifier == countryId);
+            
             if (country == null)
             {
                 SpectreConsoleHelpers.ErrorPill(Justify.Left, $"Country with id {countryId} not found");
                 SpectreConsoleHelpers.ExitPrompt(Justify.Left);
                 return;
             }
+            
             SpectreConsoleHelpers.PinkPill(Justify.Left, $"Count {count} with query filter on {country?.Name} ");
         }
         else
         {
             SpectreConsoleHelpers.PinkPill(Justify.Left, $"Count {count} w/o query filter");
         }
+
+        bool hasFilter = context.HasQueryFilter<Customer>();
+        var entityType = context.Model.FindEntityType(typeof(Customer));
+
+        var filter = context.QueryFilterDefinition<Customer>();
+        Console.WriteLine(filter);
+
 
         Console.WriteLine();
 
